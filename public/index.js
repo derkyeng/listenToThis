ef769437c6033809fd08427708adbacb8a785c30
derkyeng <dyang@hamilton.edu>
2020-06-13 09:48:11 -0500

    added post display on client side

--- public/index.js
+++ public/index.js
@@ -17,12 +17,18 @@ async function getData(){
 }
 
 function createText(value){
-    for (i = 0; i < value.length; i++){
+    for (i = 0; i < value[0].length; i++){
         const div = document.createElement('div');
+        const br = document.createElement('br');
         const titleElement = document.createElement('div');
+        const urlElement = document.createElement('div');
 
-        titleElement.textContent = value[i];
+        titleElement.textContent = value[0][i];
+        urlElement.textContent = value[1][i];
+
         div.appendChild(titleElement);
+        div.appendChild(urlElement);
+        div.appendChild(br);
         document.body.appendChild(div);
     }
 }
